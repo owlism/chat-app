fa4928c243e8f2930921e7457c5557a11278a248
owlism <galileogalilei787@gmail.com>
2024-09-01 00:52:57 +0900

    deviseに関するビューファイルの配置

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-  get 'messages/index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  devise_for :users
   root to: "messages#index"
 end
